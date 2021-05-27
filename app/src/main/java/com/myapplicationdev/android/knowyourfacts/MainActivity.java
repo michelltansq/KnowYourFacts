a2c199202f80012455fa197b3477f5eae4baf964
brandon1804 <18044455@rp.edu.sg>
2021-05-27 15:04:47 +0800

    Edited MainActivity.java

--- app/src/main/java/com/myapplicationdev/android/knowyourfacts/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts/MainActivity.java
@@ -1,14 +1,26 @@
 package com.myapplicationdev.android.knowyourfacts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.preference.PreferenceManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.crazyhitty.chdev.ks.rssmanager.RssReader;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> fragments;
+    MyFragmentPagerAdapter adapter;
+    ViewPager viewPager;
+    Button btnRL;
 
 
     @Override
@@ -16,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = findViewById(R.id.viewPager);
+        btnRL = findViewById(R.id.btnRL);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        fragments = new ArrayList<Fragment>();
+        fragments.add(new Frag1());
+        fragments.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, fragments);
+
+        viewPager.setAdapter(adapter);
+
+        btnRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
 
